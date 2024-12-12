f6c3668cb3b5881dcdb62839b18b46ef6bc41c9f
Aileen Lizet Cruz Sanchez <116921277+AileenCS@users.noreply.github.com>
2024-12-12 14:02:15 -0800

    Add files via upload

---
+++ Cliente.py
@@ -0,0 +1,33 @@
+import xmlrpc.client
+
+
+server = xmlrpc.client.ServerProxy("http://localhost:8000/")
+
+
+opciones = {
+    1: ("Sumar", server.sumar),
+    2: ("Restar", server.restar),
+    3: ("Multiplicar", server.multiplicar),
+    4: ("Dividir", server.dividir)
+}
+
+def menu():
+    print("\nCalculadora Remota")
+    for key, (nombre, _) in opciones.items():
+        print(f"{key}. {nombre}")
+    print("5. Salir")
+    return int(input("Selecciona una opción: "))
+
+while True:
+    opcion = menu()
+    if opcion == 5:
+        print("\u00a1Adiós! Mi tarea ha sido completada")
+        break
+
+    if opcion in opciones:
+        num1 = float(input("Ingresa el primer número: "))
+        num2 = float(input("Ingresa el segundo número: "))
+        nombre, funcion = opciones[opcion]
+        print(f"Resultado: {funcion(num1, num2)}")
+    else:
+        print("Opción no válida. Intenta de nuevo.")
---
+++ Servidor.py
@@ -0,0 +1,20 @@
+from xmlrpc.server import SimpleXMLRPCServer
+
+#  funciones para registrar
+funciones = {
+    "sumar": lambda a, b: a + b,
+    "restar": lambda a, b: a - b,
+    "multiplicar": lambda a, b: a * b,
+    "dividir": lambda a, b: "Error: División por cero" if b == 0 else a / b
+}
+
+# Configurar el servidor
+server = SimpleXMLRPCServer(("localhost", 9000))
+print("Servidor de calculadora XML-RPC en ejecución...")
+
+# Registrar las funciones
+for nombre, funcion in funciones.items():
+    server.register_function(funcion, nombre)
+
+# Ejecución del servidor
+server.serve_forever()
